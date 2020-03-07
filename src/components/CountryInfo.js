50a29d7009c4ac19c8430e241ac776122f0958d8
Marius Zienius <marius.zienius@gmail.com>
2020-03-07 22:49:09 +0100

    displaying all species

--- src/components/CountryInfo.js
+++ src/components/CountryInfo.js
@@ -1,12 +1,53 @@
 import React, { Component } from "react"
+import { Table } from "react-bootstrap"
 
 class CountryInfo extends Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = { species: [], loading: false }
   }
+
+  async componentDidMount() {
+    const speciesUrl =
+      process.env.REACT_APP_API_SPECIES_URL +
+      this.props.country +
+      "?token=" +
+      process.env.REACT_APP_API_KEY
+    const response = await fetch(speciesUrl)
+    const data = await response.json()
+    // console.log(data)
+    this.setState({ species: data.result, loading: false })
+  }
+
   render() {
-    return <div>{this.props.country}</div>
+    console.log(this.state.species)
+    return (
+      //   <div>test</div>
+      <div>
+        {this.state.loading && !this.state.species ? (
+          <div>Loading...</div>
+        ) : (
+          <div className="container">
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>Scientific Name</th>
+                  <th>Category</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.species.map((country, i) => (
+                  <tr key={country.scientific_name}>
+                    <td>{country.scientific_name}</td>
+                    <td>{country.category}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          </div>
+        )}
+      </div>
+    )
   }
 }
 
