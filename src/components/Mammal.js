19cdff5232180f0e82138d20c44b1e1f818ece81
Marius Zienius <marius.zienius@gmail.com>
2020-03-08 21:05:09 +0100

    rendering mammals

--- src/components/Mammal.js
+++ src/components/Mammal.js
@@ -3,10 +3,29 @@ import React, { Component } from "react"
 class Mammal extends Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = { mammals: [] }
   }
+
+  async componentDidMount() {
+    const mammalsUrl =
+      process.env.REACT_APP_API_COMPGROUP_URL +
+      "mammals?token=" +
+      process.env.REACT_APP_API_KEY
+    const response = await fetch(mammalsUrl)
+    const data = await response.json()
+    this.setState({ mammals: data.result })
+  }
+
   render() {
-    return <div>Mammal Component</div>
+    return (
+      <div>
+        {this.state.mammals.map((mammal, i) => (
+          <li key={i}>
+            {i}: {mammal.scientific_name}
+          </li>
+        ))}
+      </div>
+    )
   }
 }
 
