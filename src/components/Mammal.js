78601d9bac98d23808e8a89bece6d3492cf2f311
Marius Zienius <marius.zienius@gmail.com>
2020-03-09 20:53:12 +0100

    filter mammals form all species logic

--- src/components/Mammal.js
+++ src/components/Mammal.js
@@ -3,17 +3,47 @@ import React, { Component } from "react"
 class Mammal extends Component {
   constructor(props) {
     super(props)
-    this.state = { mammals: [] }
+    this.state = { mammals: [], loading: true }
   }
 
   async componentDidMount() {
-    const mammalsUrl =
-      process.env.REACT_APP_API_COMPGROUP_URL +
-      "mammals?token=" +
-      process.env.REACT_APP_API_KEY
-    const response = await fetch(mammalsUrl)
-    const data = await response.json()
-    this.setState({ mammals: data.result })
+    let getMammalIDs = async () => {
+      const mammalsUrl =
+        process.env.REACT_APP_API_COMPGROUP_URL +
+        "mammals?token=" +
+        process.env.REACT_APP_API_KEY
+      const mammalResponse = await fetch(mammalsUrl)
+      const mammalData = await mammalResponse.json()
+      return mammalData.result.map((mammal) => mammal.taxonid)
+    }
+
+    let getSpecies = async () => {
+      const speciesUrl =
+        process.env.REACT_APP_API_SPECIES_URL +
+        this.props.region +
+        "/page/" +
+        "0" +
+        "?token=" +
+        process.env.REACT_APP_API_KEY
+      const response = await fetch(speciesUrl)
+      const speciesData = await response.json()
+      return speciesData.result
+    }
+
+    Promise.all([getMammalIDs(), getSpecies()])
+      .then(([mamalIDs, spieces]) => {
+        let mammalsInRegion = spieces.filter((animal) => {
+          return mamalIDs.includes(animal.taxonid)
+        })
+        return mammalsInRegion
+      })
+      .then((mammals) => {
+        this.setState({
+          mammals,
+          loading: false
+        })
+      })
+      .catch((err) => console.log(`ERROR: ${err}`))
   }
 
   render() {
