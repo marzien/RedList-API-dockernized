22ae23f9e9448ae6cd8cee33deafaee5de9a6a03
Marius Zienius <marius.zienius@gmail.com>
2020-03-10 17:08:53 +0100

    consistency naming for Header component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from "react"
 import "./App.css"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import RegionList from "./components/RegionList"
-import NavBar from "./components/NavBar"
+import Header from "./components/Header"
 import Mammal from "./components/Mammal"
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      <Header />
       <Router>
         <Switch>
           <Route exact path="/" component={RegionList} />
