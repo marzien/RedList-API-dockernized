ef70aaf6417d9b6d971d2fd43ca1e260045b7ba7
Marius Zienius <marius.zienius@gmail.com>
2020-03-10 17:14:13 +0100

    header functional component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,20 @@
-import React, { Component } from "react"
+import React from "react"
 import { Navbar, Nav } from "react-bootstrap"
 
-class Header extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {}
-  }
-  render() {
-    return (
-      <div className="container">
-        <Navbar bg="light" expand="lg">
-          <Navbar.Brand href="/">IUCD RedList API</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link href="/">Home</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-      </div>
-    )
-  }
+const Header = () => {
+  return (
+    <div className="container">
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="/">IUCD RedList API</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  )
 }
 
 export default Header
