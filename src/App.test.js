b2e7120ceadd45616a311a97c5d18a36c2ac655d
Marius Zienius <marius.zienius@gmail.com>
2020-03-14 23:28:51 +0100

    first header test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from "react"
 import { render } from "@testing-library/react"
 import App from "./App"
 
-test("renders learn react link", () => {
+test("renders page header correctly", () => {
   const { getByText } = render(<App />)
-  const linkElement = getByText(/learn react/i)
+  const linkElement = getByText(/IUCD RedList API/i)
   expect(linkElement).toBeInTheDocument()
 })
